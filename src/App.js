0ac7358f1ee8d2700c0ce986c2f00c4166382ddc
Charles Odukoya <olatunjicharlesodus@gmail.com>
2023-12-23 12:51:42 +0100

    Charles Odukoya

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>HELLO PEOWORLD</div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const reactNode = document.getElementById("root");
+const root = createRoot(reactNode);
+
+root.render(<App />);
